03af317d2569156bb08d45ec0c7d7b42f0d4c6b7
Oleksandr <oleksandrhredil71@gmail.com>
2025-09-19 14:59:01 +0300

    create permissions

---
+++ backend/users/permissions.py
@@ -0,0 +1,33 @@
+from rest_framework import permissions
+
+class IsAdminUser(permissions.BasePermission):
+    
+    def has_permission(self, request, view):
+        
+        return (
+            request.user and request.user.is_authenticated 
+            and request.user.role == 'admin'
+        )
+        
+class IsAdminOnReadOnly(permissions.BasePermission):
+    
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return request.user and request.user.is_authenticated
+        
+        return (
+            request.user and request.user.is_authenticated and 
+            request.user.role == 'admin'
+        )
+        
+class IsOwnerOrAdmin(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user and request.user.is_authenticated and request.user.role == 'admin':
+            return True
+        
+        if hasattr(obj, 'user'):
+            return obj.user == request.user 
+        elif hasattr(obj, 'id'):
+            return obj.id == request.user.id
+        
+        return False
