49c2732ed2abf8c59524cc5332a98abf793ad30e
Oleksandr <oleksandrhredil71@gmail.com>
2025-09-26 12:38:20 +0300

    setup allow host gor ngrok

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
