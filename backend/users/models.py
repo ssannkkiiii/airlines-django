20b3540630ae5a8f4045924fd57d0862fd33b645
Oleksandr <oleksandrhredil71@gmail.com>
2025-09-29 12:12:00 +0300

    creating oauth2 google auth

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import google.auth.transport.requests
+import google.oauth2.id_token
 
 from django.contrib.auth.models import AbstractUser
 
@@ -11,7 +13,8 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=255, verbose_name='User first name')
     last_name = models.CharField(max_length=255, verbose_name='User last name')
     date_of_birth = models.DateField(null=True, blank=True)
-     
+    google_id = models.CharField(max_length=100, unique=True, null=True, blank=True)
+
     role = models.CharField(
         max_length=20,
         choices=UserRoles.choices,
@@ -30,3 +33,27 @@ class User(AbstractUser):
     @property
     def get_full_name(self):
         return f'{self.first_name} {self.last_name}'
+    
+    def save(self, *args, **kwargs):
+        if not self.username:
+            self.username = self.email.split('@')[0]
+        super().save(*args, **kwargs)
+        
+    @classmethod
+    def from_google_token(cls, token: str) -> "User":
+        request = google.auth.transport.requests.Request()
+        id_info = google.oauth2.id_token.verify_oauth2_token(token, request)
+
+        google_id = id_info["sub"]
+        email = id_info.get("email")
+        name = id_info.get("name")
+        picture = id_info.get("picture")
+
+        user, created = cls.objects.get_or_create(
+            google_id=google_id,
+            defaults={"username": email, "email": email, "first_name": name},
+        )
+        if picture:
+            user.avatar = picture
+            user.save(update_fields=["avatar"])
+        return user
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -8,10 +8,15 @@ urlpatterns = [
     path('logout/', views.LogoutView.as_view(), name='user-logout'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
     
+    path('google/login/', views.GoogleLoginInitView.as_view(), name='google-oauth'),
+    path('google/callback/', views.GoogleAuthCallbackView.as_view(), name='google-token'),
+      
     path('profile/', views.UserProfileView.as_view(), name='user-profile'),
     path('profile/update/', views.UserUpdateView.as_view(), name='user-profile-update'),
     
     path('users/', views.UserListView.as_view(), name='users-list'),
     path('users/<int:pk>/', views.UserDetailView.as_view(), name='user-detail'),
 ]
+
+
 
